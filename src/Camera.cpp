f1b99d5355c1dd97e1b6ec32a5bee3d0d18c6512
Trubiso <trubiso@users.noreply.github.com>
2023-02-12 20:45:20 +0100

    kjepe

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -21,14 +21,14 @@ void Camera::move_direction(MovementDirection direction, double delta_time) {
 	float velocity = this->speed * delta_time;
 	// clang-format off
 	switch (direction) {
-		case Forward: 	this->position += velocity * this->front; 		break;
-		case Backward: 	this->position -= velocity * this->front; 		break;
-		case Left: 			this->position -= velocity * this->right; 		break;
-		case Right: 		this->position += velocity * this->right; 		break;
-		case WorldUp: 	this->position += velocity * this->world_up; 	break;
-		case WorldDown: this->position -= velocity * this->world_up; 	break;
-		case Up: 				this->position += velocity * this->up; 				break;
-		case Down: 			this->position -= velocity * this->up; 				break;
+		case Forward:   this->position += velocity * this->front;    break;
+		case Backward:  this->position -= velocity * this->front;    break;
+		case Left:      this->position -= velocity * this->right;    break;
+		case Right:     this->position += velocity * this->right;    break;
+		case WorldUp:   this->position += velocity * this->world_up; break;
+		case WorldDown: this->position -= velocity * this->world_up; break;
+		case Up:        this->position += velocity * this->up;       break;
+		case Down:      this->position -= velocity * this->up;       break;
 	}
 	// clang-format on
 }
